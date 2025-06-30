836b0703bdb95127c55a245a70b809b261b84953
Shaohua Wen <shaohua.wen@gmail.com>
2025-06-30 16:28:48 +0700

    change to auto scaling group

--- outputs.tf
+++ outputs.tf
@@ -18,9 +18,11 @@ output "oc_server_public_ip" {
   value = aws_instance.oc_server.public_ip
 }
 
-output "cm_server_public_ips" {
-  value = [for instance in aws_instance.cm_server : instance.public_ip]
+output "cm_server_asg_name" {
+  description = "Name of the Auto Scaling Group"
+  value       = aws_autoscaling_group.cm_asg.name
 }
+
 output "efs_id" {
   value = aws_efs_file_system.efs.id
 }
